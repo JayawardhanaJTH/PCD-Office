56facd197f9f1348bffe472bf7258b9b502df54d
JayawardanaJTH <tiran2323@gmail.com>
2023-02-02 00:33:42 +0530

    add bot

--- support/header.php
+++ support/header.php
@@ -58,6 +58,9 @@ require_once("connection/connection.php");
 	<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
 	<link href="layout/styles/Calender.css" rel="stylesheet" type="text/css" media="all">
 	<link href="layout/styles/framework.css" rel="stylesheet" type="text/css" media="all">
+	<link href="bot/style.css" rel="stylesheet" type="text/css" media="all">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
 </head>
 
@@ -200,3 +203,6 @@ require_once("connection/connection.php");
 			</div>
 		</div>
 	<?php } ?>
+	<?php if (isset($_SESSION['logged'])) {
+		include "bot/bot.php";
+	} ?>
